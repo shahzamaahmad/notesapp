973cc061bd08a13dad96479c3235888bb089577f
Shahzama Ahmad <shahzamaahmad@rapidinnovation.dev>
2022-08-01 02:06:00 +0530

    complete challange of udemy "Edit data and rewrite json data into json file"

--- json-func.js
+++ json-func.js
@@ -1,15 +1,36 @@
 
+// Challenge
 const fs = require('fs')
-const Boook = {
-  title: 'Math',
-  author: 'Shahzama'
-}
-const data = JSON.stringify(Boook)
-// console.log(data);
-fs.writeFileSync('jsonData.json', data)
-const dataBuffer = fs.readFileSync('jsonData.json').toString()
-const buff = JSON.parse(dataBuffer)
-console.log(buff.author);
+
+
+const readData = fs.readFileSync('jsonData.json').toString()
+console.log(readData);
+const pData = JSON.parse(readData)
+console.log(pData);
+pData.name = 'ubaid'
+pData.age = 20
+pData.planet = 'Mars'
+pData.city = 'Nagina'
+const editData = JSON.stringify(pData)
+fs.writeFileSync('jsonData.json', editData)
+console.log(pData);
+
+
+
+
+// const Boook = {
+//   title: 'Math',
+//   author: 'Shahzama'
+// }
+// const data = JSON.stringify(Boook)
+// // console.log(data);
+// fs.writeFileSync('jsonData.json', data)
+// const dataBuffer = fs.readFileSync('jsonData.json').toString()
+// const buff = JSON.parse(dataBuffer)
+// console.log(buff.author);
+
+
+
 
 
 // const pdata = JSON.parse(data)
