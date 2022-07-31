0cc6153eab22d5fcc4f4b5d9a770b57c38249f6e
Shahzama Ahmad <shahzamaahmad@rapidinnovation.dev>
2022-08-01 01:49:01 +0530

    write and read json file with fs module and access the data

--- json-func.js
+++ json-func.js
@@ -1,8 +1,18 @@
+
+const fs = require('fs')
 const Boook = {
   title: 'Math',
   author: 'Shahzama'
 }
 const data = JSON.stringify(Boook)
-console.log(data);
-const pdata = JSON.parse(data)
-console.log(pdata.author);
+// console.log(data);
+fs.writeFileSync('jsonData.json', data)
+const dataBuffer = fs.readFileSync('jsonData.json').toString()
+const buff = JSON.parse(dataBuffer)
+console.log(buff.author);
+
+
+// const pdata = JSON.parse(data)
+// console.log(pdata.author);
+
+
